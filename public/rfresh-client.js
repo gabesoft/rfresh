2fc2e67882ac9df191826faf89fc881164a301c0
Gabriel Adomnicai <gabesoft@gmail.com>
2013-09-21 10:00:49 -0700

    server refactoring

--- public/rfresh-client.js
+++ public/rfresh-client.js
@@ -1,43 +1,43 @@
 (function (exports) {
-  var doc = exports.document
-      , loc = exports.location
-      , host = loc.host
-      , slice = Array.prototype.slice
-      , map = Array.prototype.map
+    var doc    = exports.document
+      , loc    = exports.location
+      , host   = loc.host
+      , slice  = Array.prototype.slice
+      , map    = Array.prototype.map
       , socket = new WebSocket('ws://' + host);
 
-  var elsByTag = function (tag) {
-        return doc.getElementsByTagName(tag);
+    var elsByTag = function (tag) {
+            return slice.call(doc.getElementsByTagName(tag));
+        };
+
+    var getScripts = function () {
+            var els = []
+                   .concat(elsByTag('script'))
+                   .concat(elsByTag('link'));
+
+            return els
+               .map(function (el) {
+                    var path = el.href || el.src;
+
+                    return {
+                        url  : path.replace(loc.origin, '')
+                      , type : el.type
+                      , tag  : el.tagName
+                    }
+                })
+               .filter(function (el) {
+                    return !el.path.match(/rfresh-client/);
+                })
+        };
+
+    exports.getScripts = getScripts;
+
+    socket.onopen = function (e) {
+        var data = JSON.stringify(getScripts());
+        socket.send(data);
     };
 
-  var getScripts = function () {
-        var els = []
-            .concat(slice.call(elsByTag('script')))
-            .concat(slice.call(elsByTag('link')));
-
-        return els
-          .map(function (el) {
-            var path = el.href || el.src;
-
-            return {
-              path: path.replace(loc.origin, '')
-              , type: el.type
-              , tag: el.tagName
-            }
-          })
-          .filter(function (el) {
-            return !el.path.match(/rfresh-client/);
-          })
+    socket.onmessage = function (e) {
+        console.log(e.data);
     };
-
-  exports.getScripts = getScripts;
-
-  socket.onopen = function (e) {
-    var data = JSON.stringify(getScripts());
-    socket.send(data);
-  };
-
-  socket.onmessage = function (e) {
-    console.log(e.data);
-  };
 })(window);
